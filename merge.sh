cad8f4462a390ba00fd0982752cb1581deed38f6
西西里卷 <zqbinary@foxmail.com>
2022-08-10 18:06:02 +0800

    merge 脚本

--- merge.sh
+++ merge.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ ! -f dist/ahk.ahk ]; then
-	mkdir dist
+	if [ ! -d dist/ahk.ahk ]; then
+		mkdir dist
+	fi
 	touch dist/ahk.ahk
-	echo '' > dist/ahk.ahk
+	echo '' >dist/ahk.ahk
 fi
-cat src/*.ahk > dist/ahk.ahk
+cat src/*.ahk >dist/ahk.ahk
